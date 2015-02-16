788766ac6d4186f98583c316630ba1fd5b7d5bd3
Xiaoohu Shen <littletiger.shen@gmail.com>
2015-02-16 19:45:23 +0800

    ..

--- src/main/java/hello/web/controller/CommodityController.java
+++ src/main/java/hello/web/controller/CommodityController.java
@@ -6,10 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+import java.awt.print.Book;
+import java.net.URI;
+
+import static java.util.Collections.singletonList;
 
 /**
  * Created by xiaohu on 2015/2/13.
@@ -19,9 +27,49 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class CommodityController {
     @Autowired
     CommodityRepository commodityRepository;
+
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public Page<Commodity> listCommodity() {
-        return commodityRepository.findAll(new PageRequest(0,3));
+    public Page<Commodity> listCommodity(Pageable page) {
+        return commodityRepository.findAll(page);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    Commodity read(@PathVariable("id") Long id) {
+        return commodityRepository.findOne(id);
+    }
+
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCommodity(@PathVariable("id") Long id, @RequestBody Commodity commodity) {
+        if (commodityRepository.exists(id)) {
+            commodity.setId(id);
+            commodityRepository.save(commodity);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<String> createCommodity(HttpServletRequest request, @RequestBody Commodity commodity) {
+        final Long id = save(commodity);
+
+        URI uri = new UriTemplate("{requestUrl}/{id}").expand(request.getRequestURL().toString(), id);
+        final HttpHeaders headers = new HttpHeaders();
+        headers.put("Location", singletonList(uri.toASCIIString()));
+        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
+    }
+
+    private Long save(Commodity commodity) {
+        commodityRepository.saveAndFlush(commodity);
+        return commodity.getId();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCommodity(@PathVariable("id") Long id) {
+        commodityRepository.delete(id);
     }
 }
--- src/main/webapp/js/jqGrid-config.js
+++ src/main/webapp/js/jqGrid-config.js
@@ -12,22 +12,42 @@ $(function() {
             },
             root: "content"
         },
-        prmNames: {
-            page: "page.page",
-            rows: "page.size",
-            sort: "page.sort",
-            order: "page.sort.dir"
-        },
+        //prmNames: {
+        //    page: "page.page",
+        //    rows: "page.size",
+        //    sort: "page.sort",
+        //    order: "page.sort.dir"
+        //},
         height: 'auto',
         autowidth: true,
         viewrecords: true,
         rowList: [10, 20, 50],
-        altRows: true,
+        altRows: false,
         sortname: "id",
         loadError: function(xhr, status, error) {
             alert(error);
         }
     });
+
+    $.extend($.jgrid.edit, {
+        closeAfterEdit: true,
+        closeAfterAdd: true,
+        ajaxEditOptions: { contentType: "application/json" },
+        mtype: 'PUT',
+        serializeEditData: function(data) {
+            delete data.oper;
+            return JSON.stringify(data);
+        }
+    });
+    $.extend($.jgrid.del, {
+        mtype: 'DELETE',
+        serializeDelData: function() {
+            return "";
+        }
+    });
+
+
+
 
 
 
